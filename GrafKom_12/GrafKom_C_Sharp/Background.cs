ea4bf1a45c3dcfb2fb1dc46317f569fb5b14e48d
Luicarios <74593350+Luicario@users.noreply.github.com>
2021-04-19 00:26:21 +0700

    Add files via upload
    Projek Kelompok 12

---
+++ GrafKom_12/GrafKom_C_Sharp/Background.cs
@@ -0,0 +1,87 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using OpenTK.Graphics.OpenGL4;
+using LearnOpenTK.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Mathematics;
+using System.Globalization;
+
+namespace GrafKom_C_Sharp
+{
+    class Background
+    {
+        // Texture coordinates range from 0.0 to 1.0, with (0.0, 0.0) representing the bottom left, and (1.0, 1.0) representing the top right
+        private readonly float[] _vertices =
+        {
+            // Position         Texture coordinates
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+        };
+        private readonly uint[] _indices =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
+        private int _elementBufferObject;
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+        private Shader _shader;
+        private Texture _texture;
+        Matrix4 transform;
+
+        public void SetupBackground()
+        {
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _elementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
+            // ada perubahan di shader filenya
+            _shader = new Shader("C:/Users/Yunat/source/repos/GrafKom_C_Sharp/GrafKom_C_Sharp/Shaders/shader_bg.vert",
+                "C:/Users/Yunat/source/repos/GrafKom_C_Sharp/GrafKom_C_Sharp/Shaders/shader_bg.frag");
+            _shader.Use();
+
+            //penambahan disini
+            var vertexLocation = _shader.GetAttribLocation("aPosition");
+
+            GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(vertexLocation);
+
+            //penambahan disini
+            var texCoordLocation = _shader.GetAttribLocation("aTexCoord");
+
+            GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(texCoordLocation);
+            _texture = Texture.LoadFromFile(
+                "C:/Users/Yunat/source/repos/GrafKom_C_Sharp/GrafKom_C_Sharp/asset/pinkbg.png");
+            _texture.Use(TextureUnit.Texture0);
+        }
+
+        public void Scale(float scale)
+        {
+            transform = transform * Matrix4.CreateScale(scale);
+        }
+
+        public void Render()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.BindVertexArray(_vertexArrayObject);
+            _texture.Use(TextureUnit.Texture0);
+            _shader.Use();
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+        }
+    }
+}
---
+++ GrafKom_12/GrafKom_C_Sharp/Mesh.cs
@@ -0,0 +1,334 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using OpenTK.Graphics.OpenGL4;
+using LearnOpenTK.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Mathematics;
+using System.Globalization;
+
+namespace GrafKom_C_Sharp
+{
+    class Mesh
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> textureVertices = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<uint> vertexIndices = new List<uint>();
+        int _vbo;
+        int _ebo;
+        int _vao;
+        Shader _shader;
+        Matrix4 transform;
+        List<Mesh> child = new List<Mesh>();
+        // Universal Function
+        public Mesh()
+        {
+
+        }
+
+        public void AddChild(Mesh i) {
+            child.Add(i);
+        }
+
+        public Mesh GetChild(int index) {
+            return child[index];
+        }
+
+        public List<Vector3> GetVertices()
+        {
+            return vertices;
+        }
+        public List<uint> GetVertexIndices()
+        {
+            return vertexIndices;
+        }
+
+        public void SetVertexIndices(List<uint> temp)
+        {
+            vertexIndices = temp;
+        }
+
+        public int GetVertexBufferObject()
+        {
+            return _vbo;
+        }
+
+        public int GetElementBufferObject()
+        {
+            return _ebo;
+        }
+
+        public int GetVertexArrayObject()
+        {
+            return _vao;
+        }
+
+        public Shader GetShader()
+        {
+            return _shader;
+        }
+
+        public Matrix4 GetTransform()
+        {
+            return transform;
+        }
+
+        public void Rotate(float rx, float ry, float rz)
+        {
+            transform = transform * Matrix4.CreateRotationX(MathHelper.DegreesToRadians(rx));
+            transform = transform * Matrix4.CreateRotationY(MathHelper.DegreesToRadians(ry));
+            transform = transform * Matrix4.CreateRotationZ(MathHelper.DegreesToRadians(rz));
+
+            foreach (var meshobj in child)
+            {
+                meshobj.Rotate(rx, ry, rz);
+            }
+        }
+        public void Scale(float scale)
+        {
+            transform = transform * Matrix4.CreateScale(scale);
+
+            foreach (var meshobj in child)
+            {
+                meshobj.Scale(scale);
+            }
+        }
+
+        public void Translate(float tx, float ty, float tz)
+        {
+            transform = transform * Matrix4.CreateTranslation(tx, ty, tz);
+
+            foreach (var meshobj in child)
+            {
+                meshobj.Translate(tx, ty, tz);
+            }
+        }
+
+        public void LoadObjectFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Unable to open \"" + path + "\", does not exist.");
+            }
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    List<string> words = new List<string>(streamReader.ReadLine().ToLower().Split(' '));
+                    words.RemoveAll(s => s == string.Empty);
+
+                    if (words.Count == 0)
+                        continue;
+
+                    string type = words[0];
+                    words.RemoveAt(0);
+
+
+                    switch (type)
+                    {
+                        case "v":
+                            // https://stackoverflow.com/questions/11202673/converting-string-to-float-in-c-sharp/11203062
+                            // https://stackoverflow.com/questions/16657090/why-does-float-parse-return-wrong-value
+                            vertices.Add(new Vector3((float.Parse(words[0], CultureInfo.InvariantCulture.NumberFormat) / 5),
+                                (float.Parse(words[1], CultureInfo.InvariantCulture.NumberFormat) / 5),
+                                (float.Parse(words[2], CultureInfo.InvariantCulture.NumberFormat) / 5)));
+                            break;
+
+                        case "vt":
+                            textureVertices.Add(new Vector3(float.Parse(words[0]), float.Parse(words[1]),
+                                                            words.Count < 3 ? 0 : float.Parse(words[2])));
+                            break;
+
+                        case "vn":
+                            normals.Add(new Vector3(float.Parse(words[0]), float.Parse(words[1]), float.Parse(words[2])));
+                            break;
+
+                        case "f":
+                            foreach (string w in words)
+                            {
+                                if (w.Length == 0)
+                                    continue;
+
+                                string[] comps = w.Split('/');
+
+                                vertexIndices.Add(uint.Parse(comps[0]) - 1);
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void SetupObject(string vert, string frag)
+        {
+            transform = Matrix4.Identity;
+
+            // box
+            _vbo = GL.GenBuffer(); // vertex buffer object
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer,
+                vertices.Count * Vector3.SizeInBytes,
+                vertices.ToArray(),
+                BufferUsageHint.StaticDraw);
+
+            _vao = GL.GenVertexArray(); // vertex array object
+            GL.BindVertexArray(_vao);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float,
+                false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            _ebo = GL.GenBuffer(); // element buffer object
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer,
+                _ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer,
+                vertexIndices.Count * sizeof(uint),
+                vertexIndices.ToArray(), BufferUsageHint.StaticDraw);
+
+            _shader = new Shader(vert, frag);
+            _shader.Use();
+
+            //PrintVertexIndeces();
+        }
+
+        public void Render()
+        {
+            _shader.Use();
+            _shader.SetMatrix4("transform", transform);
+            GL.BindVertexArray(_vao);
+            GL.DrawElements(PrimitiveType.Triangles,
+                vertexIndices.Count,
+                DrawElementsType.UnsignedInt, 0);
+
+            foreach (var meshobj in child)
+            {
+                meshobj.Render();
+            }
+        }
+
+        public void PrintVertexIndeces()
+        {
+            Console.Write("Vertex Indices: ");
+            foreach (uint i in vertexIndices)
+            {
+                Console.Write(i + " ");
+            }
+            Console.Write('\n');
+        }
+
+        public void PrintVertices()
+        {
+            Console.Write("\nVertices: \n");
+            foreach (Vector3 i in vertices)
+            {
+                Console.Write(i.X + " ");
+            }
+            Console.Write('\n');
+            foreach (Vector3 i in vertices)
+            {
+                Console.Write(i.Y + " ");
+            }
+            Console.Write('\n');
+            foreach (Vector3 i in vertices)
+            {
+                Console.Write(i.Z + " ");
+            }
+            Console.Write('\n');
+        }
+
+        // Shape Specific Function
+        // Box
+        public void CreateBoxVertices(float x, float y, float z, float len)
+        {
+            float _midX = x;
+            float _midY = y;
+            float _midZ = z;
+            float _boxLength = len;
+
+            Vector3 temp;
+
+            // point 1
+            temp.X = _midX - _boxLength / 2.0f;
+            temp.Y = _midY + _boxLength / 2.0f;
+            temp.Z = _midZ - _boxLength / 2.0f;
+            vertices.Add(temp);
+
+            // point 2
+            temp.X = _midX + _boxLength / 2.0f;
+            temp.Y = _midY + _boxLength / 2.0f;
+            temp.Z = _midZ - _boxLength / 2.0f;
+            vertices.Add(temp);
+
+            // point 3
+            temp.X = _midX - _boxLength / 2.0f;
+            temp.Y = _midY - _boxLength / 2.0f;
+            temp.Z = _midZ - _boxLength / 2.0f;
+            vertices.Add(temp);
+
+            // point 4
+            temp.X = _midX + _boxLength / 2.0f;
+            temp.Y = _midY - _boxLength / 2.0f;
+            temp.Z = _midZ - _boxLength / 2.0f;
+            vertices.Add(temp);
+
+            // point 5
+            temp.X = _midX - _boxLength / 2.0f;
+            temp.Y = _midY + _boxLength / 2.0f;
+            temp.Z = _midZ + _boxLength / 2.0f;
+            vertices.Add(temp);
+
+            // point 6
+            temp.X = _midX + _boxLength / 2.0f;
+            temp.Y = _midY + _boxLength / 2.0f;
+            temp.Z = _midZ + _boxLength / 2.0f;
+            vertices.Add(temp);
+
+            // point 7
+            temp.X = _midX - _boxLength / 2.0f;
+            temp.Y = _midY - _boxLength / 2.0f;
+            temp.Z = _midZ + _boxLength / 2.0f;
+            vertices.Add(temp);
+
+            // point 8
+            temp.X = _midX + _boxLength / 2.0f;
+            temp.Y = _midY - _boxLength / 2.0f;
+            temp.Z = _midZ + _boxLength / 2.0f;
+            vertices.Add(temp);
+
+            vertexIndices = new List<uint>{
+                // Segitiga Depan 1
+                0, 1, 2,
+                // Segitiga Depan 2
+                1, 2, 3,
+                // Segitiga Atas 1
+                0, 4, 5,
+                // Segitiga Atas 2
+                0, 1, 5,
+                // Segitiga Kanan 1
+                1, 3, 5,
+                // Segitiga Kanan 2
+                3, 5, 7,
+                // Segitiga Kiri 1
+                0, 2, 4,
+                // Segitiga Kiri 2
+                2, 4, 6,
+                // Segitiga Belakang 1
+                4, 5, 6,
+                // Segitiga Belakang 2
+                5, 6, 7,
+                // Segitiga Bawah 1
+                2, 3, 6,
+                // Segitiga Bawah 2
+                3, 6, 7
+            };
+        }
+
+    }
+}
